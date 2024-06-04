cde8a4c641cedd96adee4c64d01a928bfc033ae2
Dhruv Neb <dhruv@regentmarkets.com>
2024-06-04 17:05:50 +0400

    fix: prettier fixed4

--- src/index.js
+++ src/index.js
@@ -1,89 +1,89 @@
-import "core-js/stable";
-import { log, checkOptions, getFile, createError } from "./tools";
-import Client from "./client";
+import 'core-js/stable';
+import { log, checkOptions, getFile, createError } from './tools';
+import Client from './client';
 
 let reqId = 0;
 
 export default class DocumentUploader {
-	constructor(config) {
-		this.config = config;
-		this.clients = {};
-		this.wrapConnection();
-	}
+    constructor(config) {
+        this.config = config;
+        this.clients = {};
+        this.wrapConnection();
+    }
 
-	upload(fileOptions) {
-		const { debug = false } = this.config;
+    upload(fileOptions) {
+        const { debug = false } = this.config;
 
-		const file = getFile(fileOptions);
+        const file = getFile(fileOptions);
 
-		checkOptions(file);
+        checkOptions(file);
 
-		reqId += 1;
-		const client = new Client({ send: this.send, file, reqId });
+        reqId += 1;
+        const client = new Client({ send: this.send, file, reqId });
 
-		this.clients[reqId] = { client };
+        this.clients[reqId] = { client };
 
-		return new Promise((resolve, reject) => {
-			this.clients[reqId].promise = { resolve, reject };
-			log(debug, "Uploading started, File options:", file);
-			client.requestUpload();
-		});
-	}
+        return new Promise((resolve, reject) => {
+            this.clients[reqId].promise = { resolve, reject };
+            log(debug, 'Uploading started, File options:', file);
+            client.requestUpload();
+        });
+    }
 
-	wrapConnection() {
-		const { connection, debug = false } = this.config;
+    wrapConnection() {
+        const { connection, debug = false } = this.config;
 
-		if (!connection || connection.readyState !== 1) {
-			throw createError("ConnectionError", "Connection is not ready!");
-		}
+        if (!connection || connection.readyState !== 1) {
+            throw createError('ConnectionError', 'Connection is not ready!');
+        }
 
-		this.connection = connection;
+        this.connection = connection;
 
-		this.send = (payload) => {
-			log(debug, "<Sent>:", payload);
-			connection.send(payload);
-		};
+        this.send = (payload) => {
+            log(debug, '<Sent>:', payload);
+            connection.send(payload);
+        };
 
-		const originalOnMessage = connection.onmessage;
+        const originalOnMessage = connection.onmessage;
 
-		connection.onmessage = (response) => {
-			const { data } = response;
+        connection.onmessage = (response) => {
+            const { data } = response;
 
-			log(debug, "<Received>:", data);
+            log(debug, '<Received>:', data);
 
-			const json = JSON.parse(data);
-			if (
-				originalOnMessage &&
+            const json = JSON.parse(data);
+            if (
+                originalOnMessage &&
 				(!json.passthrough || !json.passthrough.document_upload)
-			) {
-				originalOnMessage.call(connection, response);
-				return;
-			}
+            ) {
+                originalOnMessage.call(connection, response);
+                return;
+            }
 
-			const {
-				passthrough: { document_upload: isDocumentUpload },
-			} = json;
-			if (originalOnMessage && !isDocumentUpload) {
-				originalOnMessage.call(connection, response);
-				return;
-			}
+            const {
+                passthrough: { document_upload: isDocumentUpload },
+            } = json;
+            if (originalOnMessage && !isDocumentUpload) {
+                originalOnMessage.call(connection, response);
+                return;
+            }
 
-			if (!(json.req_id in this.clients)) {
-				return;
-			}
+            if (!(json.req_id in this.clients)) {
+                return;
+            }
 
-			const { client, promise } = this.clients[json.req_id];
+            const { client, promise } = this.clients[json.req_id];
 
-			try {
-				const result = client.handleMessage(json);
-				if (result) {
-					log(debug, "Upload successful, upload info:", result);
-					promise.resolve(result);
-				}
-			} catch (e) {
-				promise.reject(e);
-				log(debug, e);
-			}
-		};
-	}
+            try {
+                const result = client.handleMessage(json);
+                if (result) {
+                    log(debug, 'Upload successful, upload info:', result);
+                    promise.resolve(result);
+                }
+            } catch (e) {
+                promise.reject(e);
+                log(debug, e);
+            }
+        };
+    }
 }

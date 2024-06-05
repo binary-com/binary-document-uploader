2a7bf64b993df21e77320681e41581cee40bde12
Dhruv Neb <dhruv@regentmarkets.com>
2024-06-05 17:13:28 +0400

    fix: prettier fixed7

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-	entry: "./src",
-	output: {
-		library: "DocumentUploader",
-		libraryTarget: "umd",
-		libraryExport: "default",
-		path: __dirname,
-		filename: "DocumentUploader.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
-		],
-	},
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new TerserPlugin({
-				terserOptions: {
-					format: {
-						comments: false,
-					},
-				},
-				extractComments: false,
-			}),
-		],
-	},
+    entry : './src',
+    output: {
+        library      : 'DocumentUploader',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
+        path         : __dirname,
+        filename     : 'DocumentUploader.js',
+    },
+    module: {
+        rules: [
+            {
+                test   : /\.js$/,
+                exclude: /node_modules/,
+                use    : {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    optimization: {
+        minimize : true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
 };

3546df39e3b6f6b057f41cbc93227ecfaaa5dd13
Dhruv Neb <dhruv@regentmarkets.com>
2024-06-05 17:21:43 +0400

    fix: prettier fixed9

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ export default class DocumentUploader {
     }
     upload(fileOptions) {
         const { debug = false } = this.config;
-
         const file = getFile(fileOptions);
         checkOptions(file);
         reqId += 1;
@@ -19,12 +18,11 @@ export default class DocumentUploader {
         return new Promise((resolve, reject) => {
             this.clients[reqId].promise = { resolve, reject };
             log(debug, 'Uploading started, File options:', file);
-            client.requestUpload();
+			client.requestUpload();
         });
     }
     wrapConnection() {
         const { connection, debug = false } = this.config;
-
         if (!connection || connection.readyState !== 1) {
             throw createError('ConnectionError', 'Connection is not ready!');
         }
@@ -33,25 +31,26 @@ export default class DocumentUploader {
             log(debug, '<Sent>:', payload);
             connection.send(payload);
         };
-
         const originalOnMessage = connection.onmessage;
-
         connection.onmessage = response => {
             const { data } = response;
-
             log(debug, '<Received>:', data);
-
             const json = JSON.parse(data);
             if (originalOnMessage && (!json.passthrough || !json.passthrough.document_upload)) {
                 originalOnMessage.call(connection, response);
                 return;
             }
-
             const { passthrough: { document_upload: isDocumentUpload } } = json;
             if (originalOnMessage && !isDocumentUpload) {
                 originalOnMessage.call(connection, response);
                 return;
-	@@ -69,7 +76,6 @@ export default class DocumentUploader {
+            }
+
+            if (!(json.req_id in this.clients)) {
+                return;
+            }
+
+            const { client, promise } = this.clients[json.req_id];
 
             try {
                 const result = client.handleMessage(json);
@@ -59,7 +58,10 @@ export default class DocumentUploader {
                 if (result) {
                     log(debug, 'Upload successful, upload info:', result);
                     promise.resolve(result);
-	@@ -80,4 +86,4 @@ export default class DocumentUploader {
+                }
+            } catch (e) {
+                promise.reject(e);
+                log(debug, e);
             }
         };
     }

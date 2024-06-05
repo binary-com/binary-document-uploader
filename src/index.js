bfff2797a05edcc89e982b42f682d88b9f0f7f40
Dhruv Neb <dhruv@regentmarkets.com>
2024-06-05 17:15:47 +0400

    fix: prettier fixed8

--- src/index.js
+++ src/index.js
@@ -1,44 +1,34 @@
 import 'core-js/stable';
 import { log, checkOptions, getFile, createError } from './tools';
 import Client from './client';
-
 let reqId = 0;
-
 export default class DocumentUploader {
     constructor(config) {
         this.config = config;
         this.clients = {};
         this.wrapConnection();
     }
-
     upload(fileOptions) {
         const { debug = false } = this.config;
 
         const file = getFile(fileOptions);
-
         checkOptions(file);
-
         reqId += 1;
         const client = new Client({ send: this.send, file, reqId });
-
         this.clients[reqId] = { client };
-
         return new Promise((resolve, reject) => {
             this.clients[reqId].promise = { resolve, reject };
             log(debug, 'Uploading started, File options:', file);
             client.requestUpload();
         });
     }
-
     wrapConnection() {
         const { connection, debug = false } = this.config;
 
         if (!connection || connection.readyState !== 1) {
             throw createError('ConnectionError', 'Connection is not ready!');
         }
-
         this.connection = connection;
-
         this.send = payload => {
             log(debug, '<Sent>:', payload);
             connection.send(payload);
@@ -46,43 +36,30 @@ export default class DocumentUploader {
 
         const originalOnMessage = connection.onmessage;
 
-        connection.onmessage = (response) => {
+        connection.onmessage = response => {
             const { data } = response;
 
             log(debug, '<Received>:', data);
 
             const json = JSON.parse(data);
-            if (
-                originalOnMessage &&
-				(!json.passthrough || !json.passthrough.document_upload)
-            ) {
+            if (originalOnMessage && (!json.passthrough || !json.passthrough.document_upload)) {
                 originalOnMessage.call(connection, response);
                 return;
             }
 
-            const {
-                passthrough: { document_upload: isDocumentUpload },
-            } = json;
+            const { passthrough: { document_upload: isDocumentUpload } } = json;
             if (originalOnMessage && !isDocumentUpload) {
                 originalOnMessage.call(connection, response);
                 return;
-            }
-
-            if (!(json.req_id in this.clients)) {
-                return;
-            }
-
-            const { client, promise } = this.clients[json.req_id];
+	@@ -69,7 +76,6 @@ export default class DocumentUploader {
 
             try {
                 const result = client.handleMessage(json);
+
                 if (result) {
                     log(debug, 'Upload successful, upload info:', result);
                     promise.resolve(result);
-                }
-            } catch (e) {
-                promise.reject(e);
-                log(debug, e);
+	@@ -80,4 +86,4 @@ export default class DocumentUploader {
             }
         };
     }
